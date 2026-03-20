6804a7a1e8b2ce96fb18321bca33c5750b976f2e
luckysemwal82-sys <luckysemwal82@gmail.com>
2026-03-20 08:52:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Bot is alive ✅')
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Web server running')
+})
 const mineflayer = require('mineflayer')
 
 function createBot() {
