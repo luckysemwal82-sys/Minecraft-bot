cb37ff2694365f85ab10229c60c26c541010e67f
luckysemwal82-sys <luckysemwal82@gmail.com>
2026-03-28 10:39:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const mineflayer = require('mineflayer')
 function createBot() {
   const bot = mineflayer.createBot({
     host: 'gfarsmp.falixsrv.me',
-    port: 27815,
+    port: 24793,
     username: 'LuckyBot123',
     auth: 'offline',
     version: '1.20.1'
